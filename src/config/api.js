d3eca26713134408fddd0559b11a1bc7f48dd703
Gosman-santoso <gosmansantoso18@gmail.com>
2020-11-01 19:04:53 +0700

    Beta 1.0.3

--- src/config/api.js
+++ src/config/api.js
@@ -1,14 +1,14 @@
 import axios from "axios";
 
 // create base URL
-export const API = axios.create({
-  baseURL: "http://localhost:5000/api/v2"
-});
+// export const API = axios.create({
+//   baseURL: "http://localhost:5000/api/v2"
+// });
 
 // netify
-// export const API = axios.create({
-//   baseURL: "https://my-literature.herokuapp.com/api/v2"
-// });
+export const API = axios.create({
+  baseURL: "https://my-literature.herokuapp.com/api/v2"
+});
 
 export const setAuthToken = token => {
   if (token) {
@@ -18,19 +18,19 @@ export const setAuthToken = token => {
   }
 };
 
-export const urlAsset = {
-  img: "http://localhost:5000/uploads/img/",
-  pdf: "http://localhost:5000/uploads/pdf/",
+// export const urlAsset = {
+//   img: "http://localhost:5000/uploads/img/",
+//   pdf: "http://localhost:5000/uploads/pdf/",
 
-  file: "http://localhost:5000/public/files/",
-  thumbnail: "http://localhost:5000/public/thumbnails/"
-};
+//   file: "http://localhost:5000/public/files/",
+//   thumbnail: "http://localhost:5000/public/thumbnails/"
+// };
 
 // netify
-// export const urlAsset = {
-//   img: "https://my-literature.herokuapp.com/src/uploads/img/",
-//   pdf: "https://my-literature.herokuapp.com/src/uploads/pdf/",
+export const urlAsset = {
+  img: "https://my-literature.herokuapp.com/src/uploads/img/",
+  pdf: "https://my-literature.herokuapp.com/src/uploads/pdf/",
 
-//   file: "https://my-literature.herokuapp.com/src/public/files/",
-//   thumbnail: "https://my-literature.herokuapp.com/src/public/thumbnails/"
-// };
+  file: "https://my-literature.herokuapp.com/src/public/files/",
+  thumbnail: "https://my-literature.herokuapp.com/src/public/thumbnails/"
+};
