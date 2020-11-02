34e7fbc17531c165690b1261aab86b780a8e6f89
Gosman-santoso <gosmansantoso18@gmail.com>
2020-11-02 10:28:52 +0700

    bug fixing

--- src/config/api.js
+++ src/config/api.js
@@ -2,35 +2,35 @@ import axios from "axios";
 
 // create base URL
 // export const API = axios.create({
-//   baseURL: "http://localhost:5000/api/v2"
+//     baseURL: "http://localhost:5000/api/v2"
 // });
 
 // netify
 export const API = axios.create({
-  baseURL: "https://my-literature.herokuapp.com/api/v2"
+    baseURL: "https://try-our-literature.herokuapp.com/api/v2/"
 });
 
 export const setAuthToken = token => {
-  if (token) {
-    API.defaults.headers.common["Authorization"] = `Bearer ${token}`;
-  } else {
-    delete API.defaults.headers.common["Authorization"];
-  }
+    if (token) {
+        API.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+    } else {
+        delete API.defaults.headers.common["Authorization"];
+    }
 };
 
 // export const urlAsset = {
-//   img: "http://localhost:5000/uploads/img/",
-//   pdf: "http://localhost:5000/uploads/pdf/",
+//     img: "http://localhost:5000/uploads/img/",
+//     pdf: "http://localhost:5000/uploads/pdf/",
 
-//   file: "http://localhost:5000/public/files/",
-//   thumbnail: "http://localhost:5000/public/thumbnails/"
+//     file: "http://localhost:5000/public/files/",
+//     thumbnail: "http://localhost:5000/public/thumbnails/"
 // };
 
 // netify
 export const urlAsset = {
-  img: "https://my-literature.herokuapp.com/uploads/img/",
-  pdf: "https://my-literature.herokuapp.com/uploads/pdf/",
+    img: "https://try-our-literature.herokuapp.com/api/v2/uploads/img/",
+    pdf: "https://try-our-literature.herokuapp.com/api/v2/uploads/pdf/",
 
-  file: "https://my-literature.herokuapp.com/public/files/",
-  thumbnail: "https://my-literature.herokuapp.com/public/thumbnails/"
+    file: "https://try-our-literature.herokuapp.com/api/v2/public/files/",
+    thumbnail: "https://try-our-literature.herokuapp.com/api/v2/public/thumbnails/"
 };
